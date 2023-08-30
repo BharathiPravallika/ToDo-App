1ec84a73ecde639723e0a9972de1f8042de20439
BharathiPravallika <bharathipravallika7@gmail.com>
2023-08-30 13:11:22 +0530

    Initial commit of todo app

---
+++ mytodoapp/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mytodoapp';
+  tasks:any = [];
+  newtask(content:string){
+    if(content != ""){
+      this.tasks.push({id:this.tasks.length, title: content})
+      console.log(this.tasks)
+    }
+  }
+  removetask(id: number){
+    this.tasks = this.tasks.filter((item:{id:number}) => item.id != id)
+  }
+  removealltasks(){
+    this.tasks = []
+  }
+}
